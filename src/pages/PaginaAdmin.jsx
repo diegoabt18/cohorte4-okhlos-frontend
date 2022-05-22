1eee3585d10ee8897413ac13b2c25a28d6abe188
JFJ1972 <97111500+JFJ1972@users.noreply.github.com>
2022-05-22 18:18:52 -0500

    actualizacion paginaadmin

--- src/pages/PaginaAdmin.jsx
+++ src/pages/PaginaAdmin.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import { Footer, NavbarDashboard, SidebarDashboard } from '../components/templates'
-import { Routes, Route, NavLink } from "react-router-dom";
-import Dashboard from './Dashboard';
-import EstudentPageLoad from './EstudentPageLoad';
-import ManageStudent from './ManageStudent';
 
 const PaginaAdmin = ({ children }) => {
     return (
