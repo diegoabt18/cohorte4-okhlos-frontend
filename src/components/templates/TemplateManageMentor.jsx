7644f8e0331205fb7d7f9a9b34c20d36761c9b4d
JFJ1972 <97111500+JFJ1972@users.noreply.github.com>
2022-05-24 19:05:51 -0500

    MentorFunctionLoad

--- src/components/templates/TemplateManageMentor.jsx
+++ src/components/templates/TemplateManageMentor.jsx
@@ -18,7 +18,7 @@ const TemplateManageMentor = () => {
     return (
             <div className='grid'>
                 <div className='pt-6 pb-4 px-4 font-Roboto'>
-                    <PageOptionsTitle text={"Administrar Estudiantes"} />
+                    <PageOptionsTitle text={"Administrar Mentores"} />
                 </div>
                 <div className='px-2'>
                     <OptionTableAdmin filter={filter.data.length? filter:dataApi} setFilter={setFilter} load={"mentor"} />
