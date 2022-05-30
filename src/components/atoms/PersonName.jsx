24a07f3e6ff8c8b50833cbc8d945e77ed1bdd4c9
JFJ1972 <97111500+JFJ1972@users.noreply.github.com>
2022-05-30 09:37:11 -0500

    updatenameuser personname menucabecera textinfoDashBoard

--- src/components/atoms/PersonName.jsx
+++ src/components/atoms/PersonName.jsx
@@ -3,10 +3,9 @@ import authServices from '../../api/services/authServices'
 
 const PersonName = () => {
   const user=authServices.isLogin()
-  console.log(user)
   return (
     <p className='text-center  font-Roboto font-bold'>
-        Diego Botello
+  {user.name}
     </p>
   )
 }
--- src/components/atoms/Rol.jsx
+++ src/components/atoms/Rol.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import authServices from "../../api/services/authServices";
 
 const Rol = () => {
+  const user = authServices.isLogin()
   return (
     <div className="text-center bg-PrimarioColor2 text-black text-md font-Roboto font-medium">
-      Admin
+      {user.role}
     </div>
   );
 };
