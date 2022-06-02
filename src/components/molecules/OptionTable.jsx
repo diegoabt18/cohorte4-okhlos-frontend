508c654a73294da6b7b68db2bbb92ac501355235
Karen <karenvane129@gmail.com>
2022-06-02 11:41:40 -0500

    Responsive

--- src/components/molecules/OptionTable.jsx
+++ src/components/molecules/OptionTable.jsx
@@ -69,9 +69,6 @@ const dispacth = useDispatch()
                 items-center
                 w-full
                 sm:flex-row
-                md:justify-end
-                
-                  
             '>
                 <ButtonLoadDataBasicExcel load={load} />
                 <ButtonIconText text={"Guardar Datos"} icon={"fluent:save-16-regular"} func={insertData}  />
